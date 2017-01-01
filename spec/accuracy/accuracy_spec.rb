45db5fbd7bb6f4189c5b29bc2a04788579a2f4ab
Chang <chang.rajani@gmail.com>
2017-01-01 16:22:23 +0200

    Refactor specs, add twofold validation

--- spec/accuracy/accuracy_spec.rb
+++ spec/accuracy/accuracy_spec.rb
@@ -2,40 +2,70 @@ require 'rails_helper'
 include Magick
 include CoffeeStates
 
-describe "Accuracy measurement" do
+describe "Measuring accuracy using" do
 
-  $error_rate = 0
-  $error = 0
+  $LOO_error_rate = 0
+  $LOO_error = 0
+  $tfold_error_rate = 0
+  $tfold_error = 0
 
-  it "is printed after all" do
-    DATA_PATH = 'lib/training_data/'
-    LABEL_PATH = 'lib/training_data/labels.json'
+  DATA_PATH = 'lib/training_data/'
+  LABEL_PATH = 'lib/training_data/labels.json'
+  IMAGE_NAMES = Dir.entries(DATA_PATH).reject{ |x|
+    x == '.' or x == '..' or x == 'labels.json'
+  }.map{|x| (DATA_PATH + x)}
+  LABELS = JSON.parse IO.read(LABEL_PATH)
 
-    image_names = Dir.entries(DATA_PATH).reject{ |x|
-      x == '.' or x == '..' or x == 'labels.json'
-    }.map{|x| (DATA_PATH + x)}
-    labels = JSON.parse IO.read(LABEL_PATH)
+  it "leave-one-out cross-validation", fast: false do
     n_misclass = 0
-    image_names.each do |test_image_name|
+    IMAGE_NAMES.each do |test_image_name|
       image = Magick::Image.read(test_image_name).first.minify
-      predicted = Knn.new.classify(image, image_names.reject{|x| x == test_image_name})
-      true_value = labels[test_image_name.split("/").last]
-      puts "#{predicted} vs #{true_value}"
+      predicted = Knn.new.classify(image, IMAGE_NAMES.reject{|x| x == test_image_name})
+      true_value = LABELS[test_image_name.split("/").last]
+      #puts "#{predicted} vs #{true_value}"
 
-      if(predicted != true_value)
+      if predicted != true_value
         n_misclass += 1
-        #special error
-        $error += (CoffeeStates.const_get(predicted) - CoffeeStates.const_get(true_value)).abs
+        # weighted error
+        $LOO_error += (CoffeeStates.const_get(predicted) - CoffeeStates.const_get(true_value)).abs
       end
     end
-    $error_rate = n_misclass.to_f / image_names.size.to_f
-    $error /= image_names.size.to_f
+    $LOO_error_rate = n_misclass.to_f / IMAGE_NAMES.size.to_f
+    $LOO_error /= IMAGE_NAMES.size.to_f
+  end
+
+  it "2-fold cross-validation", fast: true do
+    shuffled = IMAGE_NAMES.shuffle
+    # 80-20 split
+    split_point = (shuffled.size * 0.8).to_i
+    training_data = shuffled.take split_point
+    test_data = shuffled.drop split_point
+
+    n_misclass = 0
+    test_data.each do |test_image_name|
+      image = Magick::Image.read(test_image_name).first.minify
+
+      predicted = Knn.new.classify(image, training_data)
+      true_value = LABELS[test_image_name.split("/").last]
+
+      if predicted != true_value
+        n_misclass += 1
+        # weighted error
+        $tfold_error += (CoffeeStates.const_get(predicted) - CoffeeStates.const_get(true_value)).abs
+      end
+      $tfold_error_rate = n_misclass.to_f / test_data.size.to_f
+      $tfold_error /= test_data.size.to_f
+    end
+
   end
 
   #cross-validating the zero-one error with leave one out -teqhinque and special-error
   after :all do
+    puts
+    puts "error rate for leave-one-out #{$LOO_error_rate}"
+    puts "weighted error for leave-one-out #{$LOO_error}"
+    puts "error rate for leave-one-out #{$tfold_error_rate}"
+    puts "weighted error for leave-one-out #{$tfold_error}"
 
-    p "error rate #{$error_rate}"
-    p "special error #{$error}"
   end
 end
