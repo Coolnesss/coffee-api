7aa7fa16763a03fb73da112e815891563f405371
Chang <chang.rajani@gmail.com>
2017-01-27 16:38:33 +0200

    try to make whenever work

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+require "#{Whenever.path}/lib/coffee_notifier"
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
--- lib/coffee_notifier.rb
+++ lib/coffee_notifier.rb
@@ -1,3 +1,4 @@
+require 'singleton'
 class CoffeeNotifier
   include Singleton
   @@ok = true
