d76d73f252f0c78e6b3fd4ed41a4b3909c1a364d
Chang <chang.rajani@gmail.com>
2017-01-27 17:44:24 +0200

    Make the notification job work

--- lib/coffee_notifier.rb
+++ lib/coffee_notifier.rb
@@ -1,17 +1,22 @@
-require 'singleton'
+require 'json'
+
 class CoffeeNotifier
-  include Singleton
-  @@ok = true
+
+  # Strong bubble gum to get state of server without retraining model
+  $url = "localhost:3000/state" if Rails.env.development?
+  $url = "http://coffeeapi.g7xd2rhrfs.eu-central-1.elasticbeanstalk.com/state" if Rails.env.production?
+  $file_path = ".coffee_state"
 
   # To avoid spamming notifications,
   # only send another one when coffee levels have sufficiently gone down first
-  def notify
-    state = CoffeeState.state
-    if state >= 10 and @@ok
+  def self.notify
+    state = JSON.parse(RestClient.get($url))["state"]
+    previous_ok = eval(IO.read($file_path))
+    if state >= 10 and previous_ok
       Firebase.send state
-      @@ok = false
+      IO.write($file_path, "false")
     elsif state <= 3
-      @@ok = true
+      IO.write($file_path, "true")
     end
   end
 end
